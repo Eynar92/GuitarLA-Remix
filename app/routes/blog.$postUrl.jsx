b16d1399fa8882d2c8a8d2cd9178ecd868f7f651
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-06-22 02:22:27 -0400

    We added nested routes to the blog section

--- app/routes/blog.$postUrl.jsx
+++ app/routes/blog.$postUrl.jsx
@@ -1,16 +1,6 @@
 import { useLoaderData } from '@remix-run/react'
 import { getPost } from '~/models/posts.server'
 import { formatearFecha } from '~/utils/helpers'
-import styles from '~/styles/blog.css'
-
-export function links() {
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles
-        }
-    ]
-}
 
 export function meta({ data }) {
     console.log(data)
@@ -51,7 +41,7 @@ export default function Post() {
     const { titulo, contenido, imagen, publishedAt } = post?.data[0]?.attributes
 
     return (
-        <article className="contenedor post mt-3">
+        <article className="post mt-3">
             <img src={imagen?.data?.attributes?.url} alt={`Imagen blog ${titulo}`} className="imagen" />
             <div className="contenido">
                 <h3>{titulo}</h3>
